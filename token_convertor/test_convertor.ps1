08c432e05bc3f53205913122787c6c6b8ad4083e
pengfei liu <liu.pengfei@hotmail.fr>
2026-08-10 16:35:56 +0200

    update ps example

--- token_convertor/test_convertor.ps1
+++ token_convertor/test_convertor.ps1
@@ -4,7 +4,14 @@ $hadoopCp = (hdfs classpath | Out-String).Trim()
 # 2. Combine with your compiled JAR path using a semicolon ';'
 $fullCp = "$hadoopCp;target/make-creds-file-1.0.0-SNAPSHOT.jar"
 
+# to get the token string, you need to run:
+# curl -k --negotiate -u : "https://deb13-spark1.casdds.casd:50470/webhdfs/v1/?op=GETDELEGATIONTOKEN&renewer=hdfs"
+# make sure you have a valid kerberos before running this command.
+$tokenStr = "IgAHcGxpdS1hZARoZGZzAIoBn-wMxPSKAaAQGUj0jgE_j9gU6yi6RBmIDbDA2hOzqdolWkuNbAgTU1dFQkhERlMgZGVsZWdhdGlvbhAxMC41MC41LjIwMzo5MDAw"
+
+$serviceStr = "10.50.5.224:8020"
+
 # 3. Execute
 java -cp $fullCp org.casd.util.MakeCredsFile `
-  "C:\tmp\creds.bin" `
-  "HDFS_DELEGATION_TOKEN" "JQAIaG9y..." "10.0.0.1:8020" `
+  "C:\Users\pliu\Documents\git\win_hdp_token_manager\token_convertor\tmp\creds.dt" `
+  "HDFS_DELEGATION_TOKEN" "$tokenStr" "$serviceStr"
